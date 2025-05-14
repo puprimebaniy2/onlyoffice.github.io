de65f4af66f8cd50b8126498b47829c076b3a419
puprimebaniy2 <puprimebaniy2@gmail.com>
2025-05-14 11:06:34 +0300

    Update pluginCode.js

--- sdkjs-plugins/content/Highlight666Plugin/pluginCode.js
+++ sdkjs-plugins/content/Highlight666Plugin/pluginCode.js
@@ -1,26 +1,32 @@
 /* pluginCode.js */
 (function(window, undefined) {
     window.Asc.plugin.init = function() {
-        // Регистрируем обработчик события изменения таблицы
-        window.Asc.plugin.event_onWorksheetChange = function(range) {
-            var cellValue = range.GetValue();
-            if (cellValue !== null && cellValue !== undefined) {
-                var trimmedValue = cellValue.toString().trim();
-                if (trimmedValue === "666" || Number(trimmedValue) === 666) {
-                    // Создаем красный цвет
-                    var redColor = Api.CreateColorFromRGB(255, 0, 0);
-                    range.SetFillColor(redColor);
+        console.log("Плагин Highlight666Plugin инициализирован");
+        // Привязываем обработчик события onWorksheetChange
+        window.Api.attachEvent("onWorksheetChange", function(range) {
+            try {
+                console.log("Событие onWorksheetChange вызвано для ячейки: " + range.GetAddress());
+                var cellValue = range.GetValue();
+                if (cellValue !== null && cellValue !== undefined) {
+                    var trimmedValue = cellValue.toString().trim();
+                    console.log("Значение ячейки: " + trimmedValue);
+                    if (trimmedValue === "666" || Number(trimmedValue) === 666) {
+                        console.log("Обнаружено '666', окрашиваем ячейку в красный");
+                        var redColor = Api.CreateColorFromRGB(255, 0, 0);
+                        range.SetFillColor(redColor);
+                    }
+                } else {
+                    console.log("Значение ячейки пустое или undefined");
                 }
+            } catch (e) {
+                console.error("Ошибка в onWorksheetChange: " + e.message);
             }
-        };
+        });
     };
 
-    window.Asc.plugin.button = function(id) {
-        // Пустая функция для кнопки плагина (не используется)
-    };
+    window.Asc.plugin.button = function(id) {};
 
     window.Asc.plugin.destroy = function() {
-        // Очистка при отключении плагина
-        window.Asc.plugin.event_onWorksheetChange = null;
+        console.log("Плагин Highlight666Plugin уничтожен");
     };
 })(window);
